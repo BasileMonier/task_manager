fbfc226511e43a496724b2ab40be45b0e77a8c99
Basile Monier <basilemonier@gmail.com>
2026-09-22 15:41:22 +0200

    feat: ajout du php de base sur tasks.php

--- tasks.php
+++ tasks.php
@@ -5,4 +5,8 @@ if (!isset($_SESSION['user_id'])) {
     header ('Location: login.php');
     exit();
 }
+    $stmt = $db->prepare('SELECT id, user_id, titre, description, statut, date_creation FROM Tasks WHERE user_id = :user_id');
+    $stmt->execute([':user_id' => $_SESSION['user_id']]);
+    $tasks = $stmt->fetchAll();
+    var_dump($tasks);
 ?>
