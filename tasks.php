b9e17e016213ab08d94aae329b2620e4b7508926
Basile Monier <basilemonier@gmail.com>
2026-09-23 16:29:18 +0200

    fix: suppression du vardump

--- tasks.php
+++ tasks.php
@@ -8,7 +8,6 @@ if (!isset($_SESSION['user_id'])) {
     $stmt = $db->prepare('SELECT id, user_id, titre, description, statut, date_creation FROM Tasks WHERE user_id = :user_id');
     $stmt->execute([':user_id' => $_SESSION['user_id']]);
     $tasks = $stmt->fetchAll();
-    var_dump($tasks);
 ?>
 
 <!DOCTYPE html>
