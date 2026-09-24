94d4c87d5372d2fc34a511ac2badb5ffc6382f3b
Basile Monier <basilemonier@gmail.com>
2026-09-24 11:10:30 +0200

    feat: finalisation de task.js

--- JS/tasks.js
+++ JS/tasks.js
@@ -1,7 +1,28 @@
-const checkButtons = document.querySelectorAll ('.task-check');
-checkButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
+const checkButtons = document.querySelectorAll(".task-check");
+
+checkButtons.forEach((buttons) => {
+    buttons.addEventListener("click", (event) => {
         const taskId = event.currentTarget.dataset.taskId;
-        console.log("Task cliquée : ", taskId);
-    });
-});
+
+        fetch("update-task.php", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({ task_id: taskId})
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if(data_success) {
+                buttons.classList.toggle("checked");
+                const texteSpan = buttons.parentElement;querySelector(".task-text");
+                texteSpan.classList.toggle("done");
+            } else {
+                console.error(data.error);
+            }
+        })
+        .catch((error) => {
+            console.error("Request failed:", error);
+        })
+    })
+})
